139f69c5a1944bcc499618d10f8611cf96a2c17b
Moyses Azevedo <m.mirazev@gmail.com>
2023-06-07 09:21:18 -0300

    first commit

---
+++ RaidUppTV/RaidUppTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaidUppTVApp.swift
+//  RaidUppTV
+//
+//  Created by Moyses Miranda do Vale Azevedo on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RaidUppTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
